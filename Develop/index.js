a623d7b06c9d3a87c28d80b67f9d9ae87b6a5659
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-04-20 14:51:48 -0500

    Successfully created questions array and used inquirer to prompt user each question, console logging the responses

--- Develop/index.js
+++ Develop/index.js
@@ -1,32 +1,85 @@
 // Steps to solve
-    // 1. User answers questions
-        // inquirer prompt
-    // 2. Pass answers in the .then()
-        // import generatemarkdown
-    // 3. Hand off to generate markdown to create MD syntax
-    // 4. Pass MD to writetofile
+// 1. User answers questions
+// inquirer prompt
+// 2. Pass answers in the .then()
+// import generatemarkdown
+// 3. Hand off to generate markdown to create MD syntax
+// 4. Pass MD to writetofile
 
 // TODO: Include packages needed for this application
 const generateMD = require("./utils/generateMarkdown.js");
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = ["What is your username?","What is the title of your project?","Write a description of your project:", "Write instructions for how to install your project:","Which license are you using?","Write instructions for how to use your program:","How should the user test your program?"];
+// Questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is your username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Write a description of your project:",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Write instructions for how to install your project:",
+    name: "install",
+  },
+  {
+    type: "input",
+    message: "Write instructions for how to use your program:",
+    name: "howtouse",
+  },
+  {
+    type: "input",
+    message: "What guidelines would you like the user to follow for contributions?",
+    name: "contribute",
+  },
+  {
+    type: "input",
+    message: "How should the user test your program?",
+    name: "test",
+  },
+  {
+    type: "list",
+    message: "Which license are you using?",
+    name: "license",
+    choices: ["MIT","GPLv2","Apache"],
+  },
+];
+
+inquirer
+    .prompt(questions)
+    .then((answers) =>
+    console.log("user responses", answers)
+    );
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    // use fs.writefile here
-    // How do I use the markdown file here??
-    // import markdown functions
+  // use fs.writefile here
+  // How do I use the markdown file here??
+  // import markdown functions
 }
 
 // TODO: Create a function to initialize app
 function init() {
-        // Write inquirer prompt
-    // call writetofile inside .then()
-    // pass answers to writetofile here
+  // Write inquirer prompt
+  // call writetofile inside .then()
+  // pass answers to writetofile here
 }
-
 
 // Function call to initialize app
 init();
